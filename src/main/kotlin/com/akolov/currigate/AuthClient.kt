ee4ac13b2a63345e77177742a925575e7691dc43
Assen Kolov <assen.kolov@gmail.com>
2017-06-25 20:31:18 +0200

    k8s files

---
+++ src/main/kotlin/com/akolov/currigate/AuthClient.kt
@@ -0,0 +1,16 @@
+package com.akolov.currigate
+
+
+class AuthClient {
+    var clientId: String? = null
+
+    var clientSecret: String? = null
+    var accessTokenUri: String? = null
+    var userAuthorizationUri: String? = null
+    var userInfoUri: String? = null
+    var scope: List<String>? = null
+}
+
+
+
+
