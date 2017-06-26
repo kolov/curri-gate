3b16a3d6d0d8ea2d4e2528519757eab6932e592a
Assen Kolov <assen.kolov@gmail.com>
2017-06-27 00:41:36 +0200

    env vars config

--- src/main/kotlin/com/akolov/currigate/AuthClient.kt
+++ src/main/kotlin/com/akolov/currigate/AuthClient.kt
@@ -1,14 +1,26 @@
 package com.akolov.currigate
 
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.stereotype.Component
+import java.util.*
 
+
+@Component
 class AuthClient {
-    var clientId: String? = null
 
+    @Value("\${oauth2.google.client.clientId}")
+    var clientId: String? = null
+    @Value("\${oauth2.google.client.secret}")
     var clientSecret: String? = null
+    @Value("\${oauth2.google.client.accessTokenUri}")
     var accessTokenUri: String? = null
+    @Value("\${oauth2.google.client.userAuthorizationUri}")
     var userAuthorizationUri: String? = null
+    @Value("\${oauth2.google.client.userInfoUri}")
     var userInfoUri: String? = null
-    var scope: List<String>? = null
+
+    val scope: List<String> = Arrays.asList("openid","email", "profile")
+
 }
 
 
--- src/main/kotlin/com/akolov/currigate/CurriGateApplication.kt
+++ src/main/kotlin/com/akolov/currigate/CurriGateApplication.kt
@@ -2,8 +2,6 @@ package com.akolov.currigate
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.context.properties.ConfigurationProperties
-import org.springframework.context.annotation.Bean
 
 
 fun main(args: Array<String>) {
@@ -11,12 +9,7 @@ fun main(args: Array<String>) {
 }
 
 @SpringBootApplication
-
 open class CurriGateApplication() {
 
-    @Bean
-    @ConfigurationProperties("oauth2.google.client")
-    open fun google(): AuthClient {
-        return AuthClient()
-    }
+
 }
